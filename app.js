1262f250d17cf7827168002c3cad0d7959a14307
irene <irene.palomares.canales.5@gmail.com>
2022-04-27 10:34:06 +0200

    pruebas

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 // app.set('views', __dirname + '/views')
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/HTML'));
 
 /*app.use('/', require('/router/Rutas Web'));*/
 
