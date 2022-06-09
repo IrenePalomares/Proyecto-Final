75111a977444b46f4869c19e92eada41a5b4809b
irene <irene.palomares.canales.5@gmail.com>
2022-06-09 17:47:25 +0200

    resultado final

--- router/contacto.js
+++ router/contacto.js
@@ -30,7 +30,7 @@ router.post('/', async(req, res) => {
     }
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            res.status(500).send(error, message);
+            res.status(500).send(error);
         } else {
             console.log('Email enviado')
             res.status(200).json(req.body)
